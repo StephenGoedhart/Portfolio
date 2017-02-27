5cbfa38b5579e21025c14fa24dbad1633f08077c
StephenGoedhart <sdg25@hotmail.com>
2017-02-27 11:02:32 +0100

    Created the base folder structure for the website

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="css/structure.css">
+  
+</head>
+<body>
+
+</body>
+</html>
