4b64bab83711f60694da584c94f6876fa174a25c
StephenGoedhart <sdg25@hotmail.com>
2017-03-01 21:48:14 +0100

    Started working on the mobile page

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <!-- external css -->
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <!-- internal css -->
   <link rel="stylesheet" type="text/css" href="css/normalize.css">
   <link rel="stylesheet" type="text/css" href="css/structure.css">
-  
+  <link rel="stylesheet" type="text/css" href="css/mainstyle.css">
+  <link rel="stylesheet" type="text/css" href="css/mobile-menu.css">
+
+
 </head>
 <body>
+  <!-- start of absolute positioned elements -->
+  <div id="mobile-menu">
+    <div id="mobile-menu-home" class="bg-blue-normal">
+      test
+      <div id="mobile-menu-item-1" class="mobile-menu-item bg-blue-normal"></div>
+      <div id="mobile-menu-item-2" class="mobile-menu-item bg-blue-normal"></div>
+      <div id="mobile-menu-item-3" class="mobile-menu-item bg-blue-normal"></div>
+    </div>
+  </div>
+  <!-- end of absolute positioned elements -->
+  <div id="container" class="col-m-12 col-tb-11 col-d-10 black-darker">
+    <header class="col-m-12 height-m-6 black-lighter"></header>
+  </div>
 
 </body>
 </html>
