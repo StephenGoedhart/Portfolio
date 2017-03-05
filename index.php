e6a2cd02247a852a257d61459623b1a604743284
StephenGoedhart <sdg25@hotmail.com>
2017-03-05 13:48:12 +0100

    Worked on the static mobile design

--- index.php
+++ index.php
@@ -2,29 +2,40 @@
 <html>
 <head>
   <!-- external css -->
-  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
   <!-- internal css -->
   <link rel="stylesheet" type="text/css" href="css/normalize.css">
   <link rel="stylesheet" type="text/css" href="css/structure.css">
   <link rel="stylesheet" type="text/css" href="css/mainstyle.css">
   <link rel="stylesheet" type="text/css" href="css/mobile-menu.css">
 
-
 </head>
 <body>
   <!-- start of absolute positioned elements -->
-  <div id="mobile-menu">
-    <div id="mobile-menu-home" class="bg-blue-normal">
-      test
-      <div id="mobile-menu-item-1" class="mobile-menu-item bg-blue-normal"></div>
-      <div id="mobile-menu-item-2" class="mobile-menu-item bg-blue-normal"></div>
-      <div id="mobile-menu-item-3" class="mobile-menu-item bg-blue-normal"></div>
+  <div id="mobile-menu" class=" hidden-on-tb hidden-on-d">
+    <div id="mobile-menu-home" class="bg-orange-normal">
+      <span class="fa fa-bars"></span>
+      <div id="mobile-menu-item-1" class="mobile-menu-item bg-orange-normal"></div>
+      <div id="mobile-menu-item-2" class="mobile-menu-item bg-orange-normal"></div>
+      <div id="mobile-menu-item-3" class="mobile-menu-item bg-orange-normal"></div>
     </div>
   </div>
   <!-- end of absolute positioned elements -->
-  <div id="container" class="col-m-12 col-tb-11 col-d-10 black-darker">
-    <header class="col-m-12 height-m-6 black-lighter"></header>
-  </div>
+  <div class="col-tb-12 height-tb-6 bg-black-darker"> </div>
+    <!-- header -->
+    <div class="col-m-12 height-m-10 col-tb-12 height-tb-10 bg-black-normal">
+      <div class="col-m-5 height-m-10 bg-orange-normal bottom-right-border-radius-m-12"></div>
+    </div>
+    <!-- content -->
+    <div class="col-m-12 bg-white-normal">
+      <!-- item 1 -->
+      <div class="col-m-10 height-m-12 bg-black-darker float-m-middle margin-top-m-6"></div>
+      <!-- item 2 -->
+      <div class="col-m-10 height-m-12 bg-black-darker float-m-middle margin-top-m-6"></div>
+      <!-- item 3 -->
+      <div class="col-m-10 height-m-12 bg-black-darker float-m-middle margin-top-m-6"></div>
+    </div>
 
 </body>
 </html>
