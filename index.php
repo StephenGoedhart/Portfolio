7f4251ffbff59aeb7393bc4b94e7324029c7866f
StephenGoedhart <sdg25@hotmail.com>
2017-03-06 14:05:44 +0100

    Added stuff

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <!-- external css -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   <!-- internal css -->
   <link rel="stylesheet" type="text/css" href="css/normalize.css">
   <link rel="stylesheet" type="text/css" href="css/structure.css">
@@ -14,7 +14,7 @@
 <body>
   <!-- start of absolute positioned elements -->
   <div id="mobile-menu" class=" hidden-on-tb hidden-on-d">
-    <div id="mobile-menu-home" class="bg-orange-normal">
+    <div id="mobile-menu-home" class="bg-blue-normal">
       <span class="fa fa-bars"></span>
       <div id="mobile-menu-item-1" class="mobile-menu-item bg-orange-normal"></div>
       <div id="mobile-menu-item-2" class="mobile-menu-item bg-orange-normal"></div>
@@ -24,18 +24,42 @@
   <!-- end of absolute positioned elements -->
   <div class="col-tb-12 height-tb-6 bg-black-darker"> </div>
     <!-- header -->
-    <div class="col-m-12 height-m-10 col-tb-12 height-tb-10 bg-black-normal">
-      <div class="col-m-5 height-m-10 bg-orange-normal bottom-right-border-radius-m-12"></div>
+    <div id="header" class="col-m-12 height-m-10 col-tb-12 height-tb-10 bg-black-normal text-white">
+      <!-- logo -->
+      <div class="col-m-6 height-m-9 bg-orange-normal bottom-right-border-radius-m-12 float-m-left">
+        <div id="logo"></div>
+      </div>
+      <div class="height-m-4 float-m-right margin-right-m-6"><h1>Stephen <b>Goedhart</b></h1></div>
+      <div class=" height-m-7 float-m-right margin-right-m-6"><h2>sdg25@hotmail.com<h2></div>
+      <div class="col-m-6 height-m-4 float-m-left"><h2></a></h2></div>
+      <a href="https://www.linkedin.com/in/stephen-goedhart-452a4574/" target="_blank"><div class="sm-icon sm-fb fa fa-facebook"></div>
+      <a href="https://www.facebook.com/stephen.goedhart" target="_blank"><div class="sm-icon sm-in fa fa-linkedin"></div></a>
+      <a href="tel:0615960902"><div class="sm-icon sm-te fa fa-phone bg-orange-darker"></div></a>
+      <!-- seperator -->
+      <div id="seperator" class="col-m-12 height-m-4">
+        <div id="seperator-border" class="height-m-4 float-m-left bg-white-normal margin-top-m-7">    </div>
+      <div id="seperator-bar-1" class="seperator-bar height-m-4 float-m-right bg-white-normal margin-top-m-5"></div>
+      <div id="seperator-bar-2" class="seperator-bar height-m-4 float-m-right bg-white-normal margin-top-m-11"></div>
+      <div id="seperator-bar-3" class="seperator-bar height-m-4 float-m-right bg-white-normal margin-top-m-8"></div>
+      <div id="seperator-bar-4" class="seperator-bar height-m-4 float-m-right bg-white-normal margin-top-m-12"></div>
+      <div id="seperator-bar-5" class="seperator-bar height-m-4 float-m-right bg-white-normal margin-top-m-7"></div>
+      </div>
+      <!-- end of seperator -->
     </div>
+
     <!-- content -->
     <div class="col-m-12 bg-white-normal">
+      <div class="col-m-10 height-m-4"></div>
       <!-- item 1 -->
-      <div class="col-m-10 height-m-12 bg-black-darker float-m-middle margin-top-m-6"></div>
+      <div class="col-m-10 height-m-12 bg-black-darker float-m-middle"></div>
       <!-- item 2 -->
       <div class="col-m-10 height-m-12 bg-black-darker float-m-middle margin-top-m-6"></div>
       <!-- item 3 -->
       <div class="col-m-10 height-m-12 bg-black-darker float-m-middle margin-top-m-6"></div>
     </div>
+    <script>
+
+    </script>
 
 </body>
 </html>
