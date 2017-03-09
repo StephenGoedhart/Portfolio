32b8e3fd9298b53270d9a34c0ee772342d4e6dcc
StephenGoedhart <sdg25@hotmail.com>
2017-03-09 11:18:48 +0100

    added viewport meta

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- external css -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
